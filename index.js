ebd47d8ea62228d8b0c9bde1428d810e8700e276
suzikiken1 <suzikiken1@gmail.com>
2026-01-16 16:28:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'zt260116.suziki1.de5.net';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMzYwZmEyMWY4MzlkYTIyMjMyMjdiYmM0MzBmYjRjN2EiLCJ0IjoiMDQ2Nzg3ZWYtN2E5Yi00MDgyLTgzZjctM2U5NmU3MGIwYTAyIiwicyI6Ik5EQXdPV0V6WVRJdFpHVXdOeTAwTmpVekxUbG1OVFl0TnpJME9HTmxaV001WlRSbCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'bpb.yousef.isegaro.com';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'zt260116';                        // 节点名称
 
